09808d99b8d7c3fd47f1099895e5b67cd9c80e80
hlillian246 <hlillian246@gmail.com>
2020-08-25 20:55:29 -0700

    deleted all SQL

--- IDM Service/database_scripts/privilege_levels.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO privilege_levels (plevel, pname) VALUES (1, 'ROOT');
-INSERT INTO privilege_levels (plevel, pname) VALUES (2, 'ADMIN');
-INSERT INTO privilege_levels (plevel, pname) VALUES (3, 'EMPLOYEE');
-INSERT INTO privilege_levels (plevel, pname) VALUES (4, 'SERVICE');
-INSERT INTO privilege_levels (plevel, pname) VALUES (5, 'USER');
